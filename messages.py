775516f2293534e585c2ccc983af99c20e09ba8d
kwgohkw <goh_kwee_yong@hotmail.com>
2020-05-20 16:08:57 +0800

    Update messages.py

--- messages.py
+++ messages.py
@@ -1,7 +1,7 @@
 
 
 ask_for_profile = '''input your Dotabuff profile URL 
-e.g. https://www.dotabuff.com/players/350519205
+e.g. https://www.dotabuff.com/players/116585378
 : '''
 check_for_file = 'checking for existing data file'
 check_file_yes = 'file exists, isolating new links'
